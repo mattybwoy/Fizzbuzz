a912f284aaa27a46d7019d7576c79c1905584a33
Matthew Lock <matthew_lock20@hotmail.com>
2020-06-01 13:32:42 +0100

    Added Fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
     'fizz'
   elsif number % 5 == 0
     'buzz'
@@ -7,3 +9,5 @@ def fizzbuzz(number)
     number
   end
 end
+
+(1..20).each {|number| puts "#{number} --> #{fizzbuzz(number)}"}
