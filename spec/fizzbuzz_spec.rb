8daf4c87e402f710892544561833af78a0b526d9
India Rex <face940@gmail.com>
2020-06-01 13:19:44 +0100

    adds buzz if multiple of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,4 +9,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
  end
